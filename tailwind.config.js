7f4c2386b1cfb3161609a1a9ea1bc131c6c791a7
miyashiromc <mkmcmiyako@gmail.com>
2025-08-29 22:39:18 -0500

    camibo de paleta 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,40 +10,40 @@ module.exports = {
     extend: {
       colors: {
         'brand-primary': {
-          DEFAULT: '#4F46E5', // indigo-600
-          dark: '#818CF8', // indigo-400
+          DEFAULT: '#1D4ED8', // blue-700
+          dark: '#3B82F6', // blue-500
         },
         'brand-secondary': {
-          DEFAULT: '#818CF8', // indigo-400
-          dark: '#6366F1', // indigo-500
+          DEFAULT: '#0284C7', // sky-600
+          dark: '#38BDF8', // sky-400
         },
         'base-100': '#FFFFFF',
-        'base-200': '#F9FAFB',
-        'base-300': '#F3F4F6',
-        'base-content': '#1F2937',
-        'dark-base-100': '#111827',
-        'dark-base-200': '#1F2937',
-        'dark-base-300': '#374151',
-        'dark-base-content': '#F9FAFB',
+        'base-200': '#F3F4F6',
+        'base-300': '#E5E7EB',
+        'base-content': '#000000',
+        'dark-base-100': '#000000',
+        'dark-base-200': '#111827',
+        'dark-base-300': '#1F2937',
+        'dark-base-content': '#FFFFFF',
         success: {
           light: '#D1FAE5', // green-100
-          DEFAULT: '#059669', // green-600
-          dark: '#34D399', // green-400
+          DEFAULT: '#047857', // green-700
+          dark: '#22C55E', // green-500
         },
         error: {
           light: '#FEE2E2', // red-100
-          DEFAULT: '#DC2626', // red-600
-          dark: '#F87171', // red-400
+          DEFAULT: '#B91C1C', // red-700
+          dark: '#EF4444', // red-500
         },
         warning: {
           light: '#FEF3C7', // amber-100
-          DEFAULT: '#F59E0B', // amber-500
+          DEFAULT: '#D97706', // amber-600
           dark: '#FBBF24', // amber-400
         },
         info: {
           light: '#DBEAFE', // blue-100
-          DEFAULT: '#0EA5E9', // sky-500
-          dark: '#38BDF8', // sky-400
+          DEFAULT: '#0891B2', // cyan-600
+          dark: '#22D3EE', // cyan-400
         },
       },
     },
